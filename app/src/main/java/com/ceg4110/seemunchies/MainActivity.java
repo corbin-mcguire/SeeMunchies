dc5eaee8cfb3f7ca98868ea8d66f7c813bef4a7f
Corbin McGuire <mcguire.corbin@gmail.com>
2018-11-19 19:12:26 -0500

    Added functionality to store the image taken from the camera.

--- app/src/main/java/com/ceg4110/seemunchies/MainActivity.java
+++ app/src/main/java/com/ceg4110/seemunchies/MainActivity.java
@@ -1,13 +1,25 @@
 package com.ceg4110.seemunchies;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,12 +51,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button openGCamera = (Button) findViewById(R.id.openCamera);
-        openGCamera.setOnClickListener(new View.OnClickListener() {
+        Button openCamera = (Button) findViewById(R.id.openCamera);
+        openCamera.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(intent);
+
+                dispatchTakePictureIntent();
+//                Intent takePictureIntent = new Intent("android.media.action.IMAGE_CAPTURE");
+//                Uri uriSavedImage = Uri.fromFile(new File("/sdcard/flashCropped.png"));
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+//                startActivityForResult(takePictureIntent, 1);
+//                startActivity(takePictureIntent);
             }
         });
 
@@ -55,6 +73,43 @@ public class MainActivity extends AppCompatActivity {
                 // Add code here to open the gallery.
             }
         });
+    }
+
+    private final int REQUEST_TAKE_PHOTO = 1;
+
+    /**
+     * This function enables the user to take a picture.
+     */
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, 1);
+        }
+        File imageFile = null;
+        try {
+            imageFile = createImageFile();
+        } catch (IOException e) {
+            Toast toast = Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT);
+        }
+        if (imageFile!= null) {
+            Uri photoURI = FileProvider.getUriForFile(this,
+                    "com.example.android.fileprovider",
+                    imageFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            Log.i("Success", "Success!");
+        }
+    }
+
+    String currentPhotoPath;
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File directory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", directory);
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
 }
